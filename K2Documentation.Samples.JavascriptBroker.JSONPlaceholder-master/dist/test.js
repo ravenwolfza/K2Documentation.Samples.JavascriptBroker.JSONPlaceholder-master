7277587b168f94afea51774c7eaf20aa15fd7fed
Andre <andre.pretorius@k2.com>
2020-02-18 15:27:56 +0200

    Including dist folder - edit ignore file

---
+++ K2Documentation.Samples.JavascriptBroker.JSONPlaceholder-master/dist/test.js
@@ -0,0 +1,2 @@
+"use strict";require("core-js/modules/es.promise");var e,r=(e=require("ava"))&&"object"==typeof e&&"default"in e?e.default:e;require("@k2oss/k2-broker-core/test-framework"),require("./index"),r("no tests",async e=>{e.pass("there are currently no tests in this sample")});
+//# sourceMappingURL=test.js.map
